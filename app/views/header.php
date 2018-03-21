a8d2309320f0bc53ee68243c1e4bde581c28444a
Marco GESSA <Marco GESSA>
2018-03-21 15:12:16 +0100

    add navbar

---
+++ app/views/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Billet simple pour l'Alaska</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Bienvenue <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Les chapitres</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">L'auteur</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Jean Forteroche
+    </span>
+  </div>
+</nav>
