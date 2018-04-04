7e4bd3ab6ea96ab945d4553dddcdadf59e99a57f
Marco GESSA <Marco GESSA>
2018-04-05 01:24:24 +0200

    get post by id (front.php)

--- app/controllers/front.php
+++ app/controllers/front.php
@@ -1,6 +1,12 @@
 <?php
+require(APP_ROOT . 'app/models/PostManager.php');
+
 class front
 {
+    private $postManager;
+    function __construct() {
+        $this->postManager = new PostManager();
+    }
     function home()
     {
         require(APP_ROOT . 'app/views/home.php');
@@ -11,8 +17,9 @@ class front
         require(APP_ROOT . 'app/views/chapitres.php');
     }
 
-    function chapitre()
+    function chapitre($GET)
     {
+        $data = $this->postManager->getPostByID($GET['id']);
         require(APP_ROOT . 'app/views/chapitre.php');
     }
 }
