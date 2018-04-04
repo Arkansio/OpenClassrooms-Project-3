0e9b4a456d04059edf9997ee416e18cd44b093ba
Marco GESSA <Marco GESSA>
2018-04-05 00:38:23 +0200

    design chapitre.php page

--- app/views/chapitre.php
+++ app/views/chapitre.php
@@ -1,10 +1,42 @@
-<?php $title = 'Chapitre' ?>
+<?php
+$title = 'Chapitre';
+$chapterTitle = 'title';
+$content = 'test';
+?>
 
 <?php ob_start(); ?>
 
 <div class="title_left animated bounceInLeft">
-    Titre du chapitre
+    <?php echo $chapterTitle; ?>
 </div>
+
+<div class="chapterContent">
+    <div class="contentBody">
+        <?php echo $content; ?>
+    </div>
+    <div class="date">2017-05-21</div>
+
+    <div class="comments">
+        <div class="title"> Commentaires </div>
+        <?php for ($i = 1; $i <= 6; $i++): ?>
+
+            <div class="comment">
+                <div class="user">Marco</div>
+                <div class="content">
+                    commentaire de billet
+                </div>
+                <div class="dateContainer">
+                    <div class="date">
+                        Posté le 2017-07-20
+                    </div>
+                </div>
+            </div>
+
+        <?php endfor; ?>
+    </div>
+</div>
+
+
 
 <?php $content = ob_get_clean(); ?>
 
