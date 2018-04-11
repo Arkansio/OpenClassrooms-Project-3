4955ac151d9c0bbaa02cd3797b68b38c0878b64b
Marco GESSA <Marco GESSA>
2018-04-11 14:39:22 +0200

    init connection only if a PostManager function is called

--- app/models/PostManager.php
+++ app/models/PostManager.php
@@ -2,11 +2,13 @@
 class PostManager
 {
     private $bdd;
-    function __construct() {
-        $this->bdd = new PDO('mysql:host=localhost;dbname=b_alaska;charset=utf8', 'root', '');
+    function initConnection() {
+        if (!$this->bdd)
+            $this->bdd = new PDO('mysql:host=localhost;dbname=b_alaska;charset=utf8', 'root', '');
     }
     function getPostByID($id)
     {
+        $this->initConnection();
         $req = $this->bdd->prepare('SELECT * FROM billets WHERE id = ? LIMIT 1');
         $req->execute(array($id));
         return $req->fetch();
