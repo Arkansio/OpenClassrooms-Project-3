23b2703e669d184568214daadd2c186469d8a881
Marco GESSA <Marco GESSA>
2018-04-20 19:11:45 +0200

    change text

--- app/views/login.php
+++ app/views/login.php
@@ -14,7 +14,7 @@ $title = 'Login';
             <label for="password">Mot de passe</label>
             <input name="password" type="password" class="form-control" id="password" placeholder="Mot de passe">
         </div>
-        <button type="submit" class="btn btn-light">Submit</button>
+        <button type="submit" class="btn btn-light">Connexion</button>
     </form>
 </div>
 
